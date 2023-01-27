fb73541c396e339c8d4fba34f7a686257cb386ae
kclown0 <keumdy0@gmail.com>
2023-01-27 23:16:47 +0900

    servertest5

--- server/src/test/java/com/sns/SnsApplicationTests.java
+++ server/src/test/java/com/sns/SnsApplicationTests.java
@@ -3,10 +3,10 @@ package com.sns;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SnsApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
