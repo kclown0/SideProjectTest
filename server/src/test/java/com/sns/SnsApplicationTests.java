f0641f71138e8cf272ddaa65f1274beca8e56641
kclown0 <keumdy0@gmail.com>
2023-01-27 23:29:36 +0900

    test7

--- server/src/test/java/com/sns/SnsApplicationTests.java
+++ server/src/test/java/com/sns/SnsApplicationTests.java
@@ -1,20 +1,16 @@
-package com.sns;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-<<<<<<< HEAD
-//@SpringBootTest
-class SnsApplicationTests {
-
-//	@Test
-=======
-// @SpringBootTest
-class SnsApplicationTests {
-
-// 	@Test
->>>>>>> a380e244a472a6c325eabc7d757323e72932f806
-	void contextLoads() {
-	}
-
-}
+//package com.sns;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+////@SpringBootTest
+//class SnsApplicationTests {
+//
+////	@Test
+//// @SpringBootTest
+//class SnsApplicationTests {
+//
+//	// 	@Test
+//	void contextLoads() {
+//	}
+//}
